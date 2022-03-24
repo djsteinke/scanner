8a55f326ee8c448dd6437e62a8081a4e7b6ccad6
Daniel Steinke <djsteinke@gmail.com>
2022-03-24 08:14:24 -0600

    updates

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
+from time import sleep
+
 import serial
-from cv2 import VideoCapture, imshow, imwrite, waitKey, destroyWindow
+import cv2
 
 
-cam_id = 0
+cam_id = 1
 arduino_com = "COM4"
 arduino = serial.Serial(arduino_com, 115200, timeout=.1)
 
 # 200 steps/rev
 # turn table 4 steps / pic
 
-steps = 50
+steps = 5
 
 if __name__ == '__main__':
-    cam = VideoCapture(cam_id)
+    cam = cv2.VideoCapture(cam_id, cv2.CAP_ANY)
+    cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     result, image = cam.read()
+    cv2.imshow("test", image)
+    cv2.waitKey()
 
-    arduino.write("Hello from Python!")
-
+    print('Wrote test.')
     scanning = True
     step = 0
+    connected = False
 
     while scanning:
+        print(f'step{step}')
         if step == steps:
             scanning = False
         # set platform 0 deg
-        arduino.write("step")
+        if connected:
+            arduino.write(bytes("step", encoding="utf8"))
+            print('step')
         while True:
             data = arduino.readline()
-            if data == "complete":
+            strData = data.decode()
+            if len(strData) > 0:
+                print(f'rawData{strData}')
+            if bytes("setup", encoding="utf8") in data:
+                connected = True
+                break
+
+            if bytes("complete", encoding="utf8") in data:
                 result, image = cam.read()
                 if result:
                     title = f"scan_{step}"
-                    imshow(title, image)
-                    imwrite(f"scan_{step}.png", image)
-                    destroyWindow(title)
+                    cv2.imshow(title, image)
+                    cv2.imwrite(f"scan_{step}.png", image)
+                    cv2.destroyWindow(title)
                 break
         step += 1
+    cam.release()
+    cv2.destroyAllWindows()
